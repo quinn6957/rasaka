9dce88ae6a4798f49b8f66b4df60af9e3938431d
Quinn <quinn@eitgen.org>
2025-08-30 19:30:49 -0500

    Remake the website, ass a fallback and work on getting things working

---
+++ pages/vmc_request.tsx
@@ -0,0 +1,13 @@
+export default function VMCRequest() {
+  
+    // redirect to the VMC request page
+    if (typeof window !== 'undefined') {
+      window.location.href = 'https://vmc.vaiiya.com/request';
+    }
+  
+    return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800">VMC Request Page</h1>
+    </div>
+  );
+}
--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-
-import { createRoot } from "react-dom/client";
-import "./index.css";
-
-//createRoot(document.getElementById("root")!).render(<App />);
-// make routes to PrivacyPolicy and TermsOfUse work
-// e.g. /privacy and /terms
-import AppRouter from "./AppRouter.tsx";
-createRoot(document.getElementById("root")!).render(<AppRouter />);
